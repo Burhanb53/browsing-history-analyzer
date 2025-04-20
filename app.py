1610d743d998895bbeeefc2752798eebfca2814a
Burhanb53 <burhanuddinb542@gmail.com>
2025-04-20 19:37:48 +0530

    production mode

--- app.py
+++ app.py
@@ -398,4 +398,4 @@ if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     os.makedirs('uploads', exist_ok=True)
     
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
